9c000851b9ec848a0fd76948fba71f937a09a451
Karan harane <karanharane15@gmail.com>
2026-09-18 22:19:21 +0530

    Add files via upload

---
+++ 01_AreaofSq.c
@@ -0,0 +1,12 @@
+           //  AREA OF SQUARE  
+
+#include <stdio.h>
+int main() {
+    
+    float side, area;
+    printf("Enter the length of the side of the square: ");
+    scanf("%f", &side);
+    area = side * side;
+    printf("The area of the square is: %.2f\n", area);
+    return 0;
+}   
